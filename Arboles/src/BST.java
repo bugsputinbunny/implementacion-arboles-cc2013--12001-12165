c3e48d703659b3ca54377fb68cdb512acc79817a
gom12165 <gom12165@uvg.edu.gt>
2013-11-02 23:28:37 +0000

    Se agregó un método para buscar nodos al BST

--- Arboles/src/BST.java
+++ Arboles/src/BST.java
@@ -23,4 +23,28 @@ public class BST<V extends Comparable> implements ITree<V>{
     public V eliminar(V value){
         return null;
     }
+
+    protected BSNode<V> search(V value){
+        if(head != null)
+            return recursiveSearch(head, value);
+        else
+            return null;
+    }
+    protected BSNode<V> recursiveSearch(BSNode<V> start, V value){
+        if(start.getValue().compareTo(value) < 0){
+            if(start.getMayor() != null)
+                return recursiveSearch(start.getMayor(), value);
+            else
+                return null;
+        }
+        else if(start.getValue().compareTo(value) > 0){
+            if(start.getMenor() != null)
+                return recursiveSearch(start.getMenor(), value);
+            else
+                return null;
+        }
+        else{
+            return start;
+        }
+    }
 }

53ff74e5e712bc6c46a5f8fd7b7c9ab91017c506
gom12165 <gom12165@uvg.edu.gt>
2013-11-03 22:30:02 +0000

    Cambios: -Se agregó algoritmo de eliminación en BST. -Se cambiaron atributos del toString de BST y SplayTree.

--- Arboles/src/BST.java
+++ Arboles/src/BST.java
@@ -3,7 +3,7 @@
  * 
  *
  */
-
+import java.util.ArrayList;
 public class BST<V extends Comparable> implements ITree<V>{
     protected BSNode<V> head;
 
@@ -21,30 +21,105 @@ public class BST<V extends Comparable> implements ITree<V>{
     */
     @Override
     public V eliminar(V value){
-        return null;
-    }
+        //Buscar el nodo que se quiere eliminar
+        ArrayList<BSNode<V>> pareja = search(value);
 
-    protected BSNode<V> search(V value){
-        if(head != null)
-            return recursiveSearch(head, value);
+        //Si el valor esta en el arbol
+        if(pareja != null){
+            BSNode<V> parent = pareja.get(0);
+            BSNode<V> nodo = pareja.get(1);
+
+            //Si parent es null, se va a borrar la raiz del arbol
+            if(parent == null){
+                if(head.getMenor() != null){
+                    BSNode<V> maximo = getMax(head);
+                    if(! maximo.equals(head.getMenor()))
+                        maximo.setMenor(head.getMenor());
+                    maximo.setMayor(head.getMayor());
+                    head = maximo;
+                }
+                else{
+                    head = head.getMayor();
+                }
+            }
+            //Si el nodo no tiene hijos, lo borra
+            else if(nodo.getMayor()==null && nodo.getMenor()==null)
+                sustituir(parent, nodo, null);
+            //Si no tiene hijo menor
+            else if(nodo.getMenor()==null)
+                sustituir(parent, nodo, nodo.getMayor());
+            //Si no tiene hijo mayor
+            else if(nodo.getMayor()==null)
+                sustituir(parent, nodo, nodo.getMenor());
+            //Si tiene dos hijos
+            else{
+                BSNode<V> maximo = getMax(nodo);
+                sustituir(parent, nodo, maximo);
+                maximo.setMenor(nodo.getMenor());
+                maximo.setMayor(nodo.getMayor());
+            }
+            return value;
+        }
         else
             return null;
     }
-    protected BSNode<V> recursiveSearch(BSNode<V> start, V value){
-        if(start.getValue().compareTo(value) < 0){
-            if(start.getMayor() != null)
-                return recursiveSearch(start.getMayor(), value);
+    
+    public void sustituir(BSNode<V> parent, BSNode<V> prevNode, BSNode<V> newNode){
+        if(parent.getMenor().equals(prevNode))
+            parent.setMenor(newNode);
+        else
+            parent.setMayor(newNode);
+    }
+
+    protected ArrayList<BSNode<V>> search(V value){
+        if(head != null)
+            if(value.compareTo(head.getValue()) < 0)
+                return recursiveSearch(head, head.getMenor(), value);
+            else if(value.compareTo(head.getValue()) > 0)
+                return recursiveSearch(head, head.getMayor(), value);
+            else{
+                ArrayList<BSNode<V>> lista = new ArrayList(2);
+                lista.add(null);
+                lista.add(head);
+                return lista;
+            }
+        else
+            return null;
+    }
+    protected ArrayList<BSNode<V>> recursiveSearch(BSNode<V> parent, BSNode<V> nodo, V value){
+        if(nodo.getValue().compareTo(value) < 0){
+            if(nodo.getMayor() != null)
+                return recursiveSearch(nodo, nodo.getMayor(), value);
             else
                 return null;
         }
-        else if(start.getValue().compareTo(value) > 0){
-            if(start.getMenor() != null)
-                return recursiveSearch(start.getMenor(), value);
+        else if(nodo.getValue().compareTo(value) > 0){
+            if(nodo.getMenor() != null)
+                return recursiveSearch(nodo, nodo.getMenor(), value);
             else
                 return null;
         }
         else{
-            return start;
+            ArrayList<BSNode<V>> lista = new ArrayList(2);
+            lista.add(parent);
+            lista.add(nodo);
+            return lista;
+        }
+    }
+
+    protected BSNode<V> getMax(BSNode<V> nodo){
+        if(nodo.getMenor() != null){
+            BSNode<V> maximo = recursiveMax(nodo, nodo.getMayor());
+            return maximo;
+        }
+        return null;
+    }
+    protected BSNode<V> recursiveMax(BSNode<V> parent, BSNode<V> nodo){
+        if(nodo.getMayor() != null)
+            return recursiveMax(nodo, nodo.getMayor());
+        else{
+            parent.setMayor(nodo.getMenor());
+            return nodo;
         }
     }
 
@@ -67,7 +142,7 @@ public class BST<V extends Comparable> implements ITree<V>{
     /*
      * Metodo que recorre el arbol preOrder adjuntando el toString de cada nodo
      */
-    private String recursiveToString(BSNode<V> nodo){
+    protected String recursiveToString(BSNode<V> nodo){
         boolean left, right;
         String mensaje = "";
 
